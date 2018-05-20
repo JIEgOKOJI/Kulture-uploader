4d41669a39698203eb61eee2070b6aa5d90ebb67
DESKTOP-E6QQNO1\jiego <jiegokoji@gmail.com>
2018-05-20 08:33:56 +0300

    resolution detect

--- main.go
+++ main.go
@@ -34,48 +34,63 @@ func upload(c echo.Context) error {
 	s := strings.Split(file.Filename, ".")
 	path := "/zfspool/video/" + s[0] + "/"
 	previewpath := "/zfspool/previews/" + s[0] + "/"
-	ext := exists(path)
+	/*ext :=*/ exists(path)
 	exists(previewpath)
-	if !ext {
-		dst, err := os.Create(path + file.Filename)
-		if err != nil {
-			return err
-		}
-		defer dst.Close()
+	//if !ext {
+	dst, err := os.Create(path + file.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
 
-		// Copy
-		if _, err = io.Copy(dst, src); err != nil {
-			return err
-		}
-		ss := strings.Split(path+file.Filename, ".")
-		d := strings.Split(file.Filename, ".")
-		path720 := string(ss[0] + ".720p.mp4")
-		path576 := string(ss[0] + ".576p.mp4")
-		path360 := string(ss[0] + ".360p.mp4")
-		path240 := string(ss[0] + ".240p.mp4")
-		path144 := string(ss[0] + ".144p.mp4")
-		previewfile := string(previewpath + s[0] + ".png")
-		go transcode(path+file.Filename, path720, path576, path360, path240, path144, previewfile)
-		manifest := "/video/" + d[0] + "/" + d[0] + ".,144p,240p,360p,576p,720p,.mp4.urlset/master.m3u8"
-		playlists := make(map[string]string)
-		playlists["720p"] = "/video/" + d[0] + "/" + d[0] + ".720p.mp4/index.m3u8"
-		playlists["576p"] = "/video/" + d[0] + "/" + d[0] + ".576p.mp4/index.m3u8"
-		playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
-		playlists["240p"] = "/video/" + d[0] + "/" + d[0] + ".240p.mp4/index.m3u8"
-		playlists["144p"] = "/video/" + d[0] + "/" + d[0] + ".144p.mp4/index.m3u8"
-		preview := "/previews/" + d[0] + "/" + d[0] + ".png"
-		jsonObj := gabs.New()
-		jsonObj.Set(true, "status")
-		jsonObj.Set(file.Filename, "name")
-		jsonObj.Set(manifest, "manifest")
-		jsonObj.Set(playlists, "playlists")
-		jsonObj.Set(preview, "preview")
-		return c.HTML(http.StatusOK, fmt.Sprintf(jsonObj.String()))
-	} else {
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	ss := strings.Split(path+file.Filename, ".")
+	d := strings.Split(file.Filename, ".")
+	path720 := string(ss[0] + ".720p.mp4")
+	path576 := string(ss[0] + ".576p.mp4")
+	path360 := string(ss[0] + ".360p.mp4")
+	path240 := string(ss[0] + ".240p.mp4")
+	path144 := string(ss[0] + ".144p.mp4")
+	previewfile := string(previewpath + s[0] + ".png")
+	SourceHeight := getResolution(path + file.Filename)
+	//fmt.Println(SourceHeight)
+	switch {
+	case SourceHeight >= 1080:
+		fmt.Println("Will code in 4 qualities")
+	case SourceHeight >= 720:
+		fmt.Println("Will code in 3 qualities")
+	case SourceHeight >= 480:
+		fmt.Println("Will code in 2 qualities")
+	case SourceHeight >= 360:
+		fmt.Println("Will code in 1 qualities")
+	default:
+		fmt.Print("less than, Will code in 1 qualities")
+
+	}
+	go transcode(path+file.Filename, path720, path576, path360, path240, path144, previewfile)
+	manifest := "/video/" + d[0] + "/" + d[0] + ".,144p,240p,360p,576p,720p,.mp4.urlset/master.m3u8"
+	playlists := make(map[string]string)
+	playlists["720p"] = "/video/" + d[0] + "/" + d[0] + ".720p.mp4/index.m3u8"
+	playlists["576p"] = "/video/" + d[0] + "/" + d[0] + ".576p.mp4/index.m3u8"
+	playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
+	playlists["240p"] = "/video/" + d[0] + "/" + d[0] + ".240p.mp4/index.m3u8"
+	playlists["144p"] = "/video/" + d[0] + "/" + d[0] + ".144p.mp4/index.m3u8"
+	preview := "/previews/" + d[0] + "/" + d[0] + ".png"
+	jsonObj := gabs.New()
+	jsonObj.Set(true, "status")
+	jsonObj.Set(file.Filename, "name")
+	jsonObj.Set(manifest, "manifest")
+	jsonObj.Set(playlists, "playlists")
+	jsonObj.Set(preview, "preview")
+	return c.HTML(http.StatusOK, fmt.Sprintf(jsonObj.String()))
+	/*} else {
 		jsonObj := gabs.New()
 		jsonObj.Set(false, "status")
 		return c.HTML(http.StatusOK, fmt.Sprintf(jsonObj.String()))
-	}
+	}*/
 
 }
 func transcode(path string, path720 string, path576 string, path360 string, path240 string, path144 string, preview string) {
@@ -115,6 +130,15 @@ func transcode(path string, path720 string, path576 string, path360 string, path
 	if err != nil {
 		fmt.Println("Error while removing: ", path, " ", err)
 	}*/
+}
+func getResolution(file string) float64 {
+	output, _ := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", file).Output()
+	var jsonParsed *gabs.Container
+	jsonParsed, _ = gabs.ParseJSON(output)
+	ffprobeParsed, _ := jsonParsed.Path("streams").Children()
+	children, _ := ffprobeParsed[0].ChildrenMap()
+	height := children["height"].Data().(float64)
+	return height
 }
 func exists(path string) bool {
 	_, err := os.Stat(path)
