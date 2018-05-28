a2a3127e589775f78e70a64f398b8d840ccd1402
DESKTOP-E6QQNO1\jiego <jiegokoji@gmail.com>
2018-05-28 14:38:10 +0300

    generate qualites lower that source file, show percentage of completion, save logs

--- main.go
+++ main.go
@@ -65,32 +65,43 @@ func upload(c echo.Context) error {
 	SourceHeight, SourceFrames := getResolution(path + file.Filename)
 	fmt.Println(SourceFrames)
 	var qualities int
+	playlists := make(map[string]string)
+	var manifest string
 	switch {
 	case SourceHeight >= 1080:
 		qualities = 4
+		playlists["1080p"] = "/video/" + d[0] + "/" + d[0] + ".1080p.mp4/index.m3u8"
+		playlists["720p"] = "/video/" + d[0] + "/" + d[0] + ".720p.mp4/index.m3u8"
+		playlists["480p"] = "/video/" + d[0] + "/" + d[0] + ".480p.mp4/index.m3u8"
+		playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
+		manifest = "/video/" + d[0] + "/" + d[0] + ".,360p,480p,720p,1080p,.mp4.urlset/master.m3u8"
 		fmt.Println("Will code in 4 qualities")
 	case SourceHeight >= 720:
 		qualities = 3
+		playlists["720p"] = "/video/" + d[0] + "/" + d[0] + ".720p.mp4/index.m3u8"
+		playlists["480p"] = "/video/" + d[0] + "/" + d[0] + ".480p.mp4/index.m3u8"
+		playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
+		manifest = "/video/" + d[0] + "/" + d[0] + ".,360p,480p,720p,.mp4.urlset/master.m3u8"
 		fmt.Println("Will code in 3 qualities")
 	case SourceHeight >= 480:
+		playlists["480p"] = "/video/" + d[0] + "/" + d[0] + ".480p.mp4/index.m3u8"
+		playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
+		manifest = "/video/" + d[0] + "/" + d[0] + ".,360p,480p,.mp4.urlset/master.m3u8"
 		qualities = 2
 		fmt.Println("Will code in 2 qualities")
 	case SourceHeight >= 360:
 		qualities = 1
+		playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
+		manifest = "/video/" + d[0] + "/" + d[0] + ".,360p,.mp4.urlset/master.m3u8"
 		fmt.Println("Will code in 1 qualities")
 	default:
 		qualities = 1
+		playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
+		manifest = "/video/" + d[0] + "/" + d[0] + ".,360p,.mp4.urlset/master.m3u8"
 		fmt.Print("less than, Will code in 1 qualities")
 
 	}
 	go transcode(path+file.Filename, qualities, path1080, path720, path480, path360, previewfile)
-	manifest := "/video/" + d[0] + "/" + d[0] + ".,144p,240p,360p,576p,720p,.mp4.urlset/master.m3u8"
-	playlists := make(map[string]string)
-	playlists["720p"] = "/video/" + d[0] + "/" + d[0] + ".720p.mp4/index.m3u8"
-	playlists["576p"] = "/video/" + d[0] + "/" + d[0] + ".576p.mp4/index.m3u8"
-	playlists["360p"] = "/video/" + d[0] + "/" + d[0] + ".360p.mp4/index.m3u8"
-	playlists["240p"] = "/video/" + d[0] + "/" + d[0] + ".240p.mp4/index.m3u8"
-	playlists["144p"] = "/video/" + d[0] + "/" + d[0] + ".144p.mp4/index.m3u8"
 	preview := "/previews/" + d[0] + "/" + d[0] + ".png"
 	jsonObj := gabs.New()
 	jsonObj.Set(true, "status")
@@ -108,44 +119,302 @@ func upload(c echo.Context) error {
 }
 func transcode(path string, qualities int, path1080 string, path720 string, path480 string, path360 string, preview string) {
 	fmt.Println(path, preview)
-	var stdoutBuf /*stderrBuf*/ bytes.Buffer
-	var errStdout, errStderr error
-	c1080 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "8000k", "-preset:v", "veryfast", "-s", "1920x1080", "-aspect", "16:9", "-f", "mp4", path1080)
-	stdoutIn, _ := c1080.StdoutPipe()
-	stderrIn, _ := c1080.StderrPipe()
-	stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
-	//	stderr := io.MultiWriter(os.Stderr, &stderrBuf)
-	c1080.Start()
-	go func() {
-		_, errStdout = io.Copy(stdout, stdoutIn)
-	}()
+	switch qualities {
+	case 4:
+		//transcode 1080
+		func() {
+			writePrgoress(4, 0, path)
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c1080 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "8000k", "-preset:v", "veryfast", "-s", "1920x1080", "-aspect", "16:9", "-f", "mp4", path1080)
+			stdoutIn, _ := c1080.StdoutPipe()
+			stderrIn, _ := c1080.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c1080.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path1080 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
 
-	go func() {
-		//_, errStderr = io.Copy(stderr, stderrIn)
-		f, _ := os.Create("log")
-		//w := //bufio.NewWriter(f)
-		w := io.Writer(f)
-		io.Copy(w, stderrIn)
+			}()
+			err := c1080.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(4, 1, path)
+		}()
+		//transcode 720
+		func() {
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c720 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "5000k", "-preset:v", "veryfast", "-s", "1280x720", "-aspect", "16:9", "-f", "mp4", path720)
+			stdoutIn, _ := c720.StdoutPipe()
+			stderrIn, _ := c720.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c720.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path720 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
 
-	}()
+			}()
+			err := c720.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(4, 2, path)
+		}()
+		//transcode 480
+		func() {
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c480 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "2500k", "-preset:v", "veryfast", "-s", "720x480", "-aspect", "16:9", "-f", "mp4", path480)
+			stdoutIn, _ := c480.StdoutPipe()
+			stderrIn, _ := c480.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c480.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path480 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
 
-	err := c1080.Wait()
-	if err != nil {
-		//fmt.Fatalf("cmd.Run() failed with %s\n", err)
+			}()
+			err := c480.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(4, 3, path)
+		}()
+		//transcode 360
+		func() {
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c360 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "1000k", "-preset:v", "veryfast", "-s", "480x360", "-aspect", "16:9", "-f", "mp4", path360)
+			stdoutIn, _ := c360.StdoutPipe()
+			stderrIn, _ := c360.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c360.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path360 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c360.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(4, 4, path)
+		}()
+	case 3:
+		//transcode 720
+		func() {
+			writePrgoress(3, 0, path)
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c720 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "5000k", "-preset:v", "veryfast", "-s", "1280x720", "-aspect", "16:9", "-f", "mp4", path720)
+			stdoutIn, _ := c720.StdoutPipe()
+			stderrIn, _ := c720.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c720.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path720 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c720.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(3, 1, path)
+		}()
+		//transcode 480
+		func() {
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c480 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "2500k", "-preset:v", "veryfast", "-s", "720x480", "-aspect", "16:9", "-f", "mp4", path480)
+			stdoutIn, _ := c480.StdoutPipe()
+			stderrIn, _ := c480.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c480.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path480 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c480.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(3, 2, path)
+		}()
+		//transcode 360
+		func() {
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c360 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "1000k", "-preset:v", "veryfast", "-s", "480x360", "-aspect", "16:9", "-f", "mp4", path360)
+			stdoutIn, _ := c360.StdoutPipe()
+			stderrIn, _ := c360.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c360.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path360 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c360.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(3, 3, path)
+		}()
+	case 2:
+		//transcode 480
+		func() {
+			writePrgoress(2, 0, path)
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c480 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "2500k", "-preset:v", "veryfast", "-s", "720x480", "-aspect", "16:9", "-f", "mp4", path480)
+			stdoutIn, _ := c480.StdoutPipe()
+			stderrIn, _ := c480.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c480.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path480 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c480.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(2, 1, path)
+		}()
+		//transcode 360
+		func() {
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c360 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "1000k", "-preset:v", "veryfast", "-s", "480x360", "-aspect", "16:9", "-f", "mp4", path360)
+			stdoutIn, _ := c360.StdoutPipe()
+			stderrIn, _ := c360.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c360.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path360 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c360.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(2, 2, path)
+		}()
+	case 1:
+		//transcode 360
+		func() {
+			writePrgoress(1, 0, path)
+			var stdoutBuf bytes.Buffer
+			var errStdout, errStderr error
+			c360 := exec.Command("ffmpeg", "-y", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "1000k", "-preset:v", "veryfast", "-s", "480x360", "-aspect", "16:9", "-f", "mp4", path360)
+			stdoutIn, _ := c360.StdoutPipe()
+			stderrIn, _ := c360.StderrPipe()
+			stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+			c360.Start()
+			go func() {
+				_, errStdout = io.Copy(stdout, stdoutIn)
+			}()
+			go func() {
+				f, _ := os.Create(path360 + ".log")
+				w := io.Writer(f)
+				io.Copy(w, stderrIn)
+
+			}()
+			err := c360.Wait()
+			if err != nil {
+				fmt.Println("cmd.Run() failed with %s\n", err)
+			}
+			if errStdout != nil || errStderr != nil {
+				fmt.Println("failed to capture stdout or stderr\n")
+			}
+			writePrgoress(1, 1, path)
+		}()
 	}
-	if errStdout != nil || errStderr != nil {
-		//fmt.Fatal("failed to capture stdout or stderr\n")
+	//genpreview
+	func() {
+
+	}()
+}
+func writePrgoress(need int, done int, name string) {
+	percentage := 100 / need
+	percentage = percentage * done
+	fmt.Println(percentage)
+	if percentage == 99 {
+		percentage = 100
 	}
-	//outStr, _ := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
-	/*f, err := os.Create("log")
-	w := bufio.NewWriter(f)
-	fmt.Fprintf(w, stderrBuf.String())
-	w.Flush()*/
-	//c720, _ := exec.Command("ffmpeg", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "5000k", "-preset:v", "veryfast", "-s", "1280x720", "-aspect", "16:9", "-f", "mp4", path720).Output()
-	//c480, _ := exec.Command("ffmpeg", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "2500k", "-preset:v", "veryfast", "-s", "720x480", "-aspect", "16:9", "-f", "mp4", path480).Output()
-	//c360, _ := exec.Command("ffmpeg", "-i", path, "-c:v", "libx264", "-c:a", "aac", "-b:a", "384k", "-b:v", "1000k", "-preset:v", "veryfast", "-s", "480x360", "-aspect", "16:9", "-f", "mp4", path360).Output()
-	//cprev := exec.Command("ffmpeg", "-i", path, "-an", "-ss", "00:00:00", "-an", "-r", "1", "-vframes", "1", "-s", "720x480", "-aspect", "16:9", preview)
-	//fmt.Println(c1080)
+	jsonObj := gabs.New()
+	jsonObj.Set(name, "filename")
+	jsonObj.Set(percentage, "done")
+	fmt.Println(jsonObj.String())
 }
 func getResolution(file string) (float64, int) {
 	output, _ := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", file).Output()
